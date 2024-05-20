278b46bfdfcb85b5c3f6ea280fc6b63afca39ffd
Chuev Savva <savvachuev@gmail.com>
2024-05-20 22:20:28 +0300

    added problems.py & solutions.py & hid token

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import asyncio
+import os
+
 from aiogram import Bot, Dispatcher
 
 from app.handlers import router
@@ -7,7 +9,7 @@ from app.database.models import async_main
 
 async def main():
     await async_main()
-    bot = Bot(token="7004003107:AAFDoI6W3NJL9x_4fIlB9zCyO9bQ0n9AreE")
+    bot = Bot(token=os.getenv("BOT_TOKEN"))
     dp = Dispatcher()
     dp.include_router(router)
     await dp.start_polling(bot)
